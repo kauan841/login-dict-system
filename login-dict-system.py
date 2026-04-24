6d9346af4ae5560748a3751e03530fd14a9e4499
kauan moraes <kauanmoraesouza@gmail.com>
2026-04-24 18:07:21 -0300

    login-dict-system

---
+++ login-dict-system.py
@@ -0,0 +1,52 @@
+usuarios = {}
+
+
+def cadastro_usuario():
+    nome = input("Digite o nome de usuário: ").strip()
+    senha = input("Digite a senha: ").strip()
+
+    if not nome or not senha:
+        print("Preencha todos os campos.")
+        return
+
+    if nome in usuarios:
+        print("Usuário já existe.")
+    else:
+        usuarios[nome] = senha
+        print("Usuário registrado com sucesso!")
+
+
+def login_usuario():
+    nome = input("Digite o nome de usuário: ").strip()
+    senha = input("Digite a senha: ").strip()
+
+    if not nome or not senha:
+        print("Preencha todos os campos.")
+        return
+
+    if nome in usuarios and usuarios[nome] == senha:
+        print("Login realizado com sucesso!")
+    else:
+        print("Nome de usuário ou senha incorretos.")
+
+
+def main():
+    while True:
+        print("\n1. Cadastrar-se")
+        print("2. Login")
+        print("3. Sair")
+
+        escolha = input("Escolha uma opção: ")
+
+        if escolha == '1':
+            cadastro_usuario()
+        elif escolha == '2':
+            login_usuario()
+        elif escolha == '3':
+            print("Saindo do sistema. Até mais!")
+            break
+        else:
+            print("Opção inválida. Tente novamente.")
+
+
+main()
